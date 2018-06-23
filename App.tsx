9d1d71abdb3c6bb781cd1ac97777c7bbf076ad1f
Sue Park <cherishgreentea@gmail.com>
2018-06-22 18:56:05 -0700

    Updated UI

--- App.tsx
+++ App.tsx
@@ -14,8 +14,7 @@ import {
 } from 'react-native';
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload i love sue,\n' +
-    'Cmd+D or shake for dev menu',
+  ios: `hello it's sue`,
   android: 'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
